72785105e66e53257116644fa7c9c22f36904253
Jahongir Rahmonov <jrahmonov2@gmail.com>
2019-02-09 10:36:31 +0500

    Initial commit. Gunicorn test.

---
+++ alcazar/api.py
@@ -0,0 +1,11 @@
+
+
+def app(environ, start_response):
+    data = b'Hello, World! I am Alcazar\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
