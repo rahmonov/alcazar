9a5a2bec642f5f5a8971191baa3a2ace1ff7f803
Jahongir Rahmonov <jrahmonov2@gmail.com>
2019-03-24 21:28:38 +0500

    added tests for middleware

---
+++ tests/test_middleware.py
@@ -0,0 +1,31 @@
+from alcazar.middleware import Middleware
+from utils import url
+
+
+def test_middleware_methods_are_called(app, client):
+    process_request_called = False
+    process_response_called = False
+
+    class CallMiddlewareMethods(Middleware):
+
+        def __init__(self, app):
+            super().__init__(app)
+
+        def process_request(self, req):
+            nonlocal process_request_called
+            process_request_called = True
+
+        def process_response(self, req, resp):
+            nonlocal process_response_called
+            process_response_called = True
+
+    app.add_middleware(CallMiddlewareMethods)
+
+    @app.route('/')
+    def index(req, res):
+        res.text = "YOLO"
+
+    client.get(url('/'))
+
+    assert process_request_called is True
+    assert process_response_called is True
